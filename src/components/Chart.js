c66dc189cd911f4f89b030c7b9668c206e28a7b8
nikomackk <nikomakin@gmail.com>
2022-05-11 16:11:00 +0300

    Chart.js korjattu, toimii

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -4,6 +4,10 @@ import lodash from 'lodash';
 
 export default function Chart() {
    const [trainings, setTrainings] = React.useState([]);
+   const [newTrainings, setNewTrainings] = React.useState({
+      activity: '',
+      totalduration: ''
+   });
 
    useEffect(() => getTrainings(), []);
 
@@ -13,14 +17,24 @@ export default function Chart() {
       .then(responseData => setTrainings(responseData))  
    }
 
-
+   let newData = 
+      lodash(trainings)
+         .groupBy('activity')
+         .map((value, key) => ({
+            'activity': key,
+            'total': lodash.sumBy(value, 'duration')
+         }))
+         .value();
+         
+   console.log(newData);
+   
 
    return (
-      <BarChart width={730} height={250} data={trainings} >
+      <BarChart width={730} height={250} data={newData} >
          <CartesianGrid strokeDasharray="3 3" />
          <XAxis dataKey="activity" />
          <YAxis label={{value: 'Kesto (min)', position: 'insideLeft', angle: 90}}/>
-         <Bar dataKey="duration" fill="black" />
+         <Bar dataKey="total" fill="black" />
       </BarChart>
    );
 }
